4b94c2c86d2c616cae7869ff3d1b02783bdc9173
Mostafa Fawzy <57078772+M0stafa-Fawzy@users.noreply.github.com>
2022-12-21 16:29:09 +0200

    initial and first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/// <reference types="@types/google.maps" />
+
+import { User } from './User'
+import { Company } from './Company'
+
+let user = new User()
+let com = new Company()
+
+console.log(user, com);
+
